64aeceef564783af9d57c0086f0dd186a5c04613
Satish <sat116@gmail.com>
2025-09-23 17:40:50 -0400

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Trump: Hey Musk, How are you?"
-echo "Musk: Hello Mr. President, I am fine. How are you doing? "
-echo "Trump: I am fine too. What's up?"
-echo "Musk: Nothing, just going to Mars now, will calyou come"
-echo: "Trump: Sorry, you carry on! I will come with your next visit"
+echo "Trump:: Hey Musk, How are you?"
+echo "Musk:: Hello Mr. President, I am fine. How are you doing? "
+echo "Trump:: I am fine too. What's up?"
+echo "Musk:: Nothing, just going to Mars now, will calyou come"
+echo "Trump:: Sorry, you carry on! I will come with your next visit"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Must
+echo "$PERSON1: Hey $PERSON2, How are you?"
+echo "$PERSON2: Hello Mr. President, I am fine. How are you doing? "
+echo "$PERSON1: I am fine too. What's up?"
+echo "$PERSON2: Nothing, just going to Mars now, will calyou come"
+echo: "$PERSON1: Sorry, you carry on! I will come with your next visit"
