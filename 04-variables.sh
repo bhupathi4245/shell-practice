b456eaf1f72c4756f737d33029eb163480001dfc
Satish <sat116@gmail.com>
2025-09-23 18:25:56 -0400

    hello world started

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-PERSON1=MODI
-PERSON2=MUSK
-echo "$PERSON1:: Hey $PERSON2, How are you?"
-echo "$PERSON2:: Hello Mr. $PERSON1, I am fine. How are you doing? "
-echo "$PERSON1:: I am fine too. What's up?"
-echo "$PERSON2:: Nothing, just going to Mars now, will calyou come"
-echo "$PERSON1:: Sorry, you carry on! I will come with your next visit"
+echo "Enter your pin number:: "
+read -s PIN  # here PIN is variable
+echo "Your number is: $PIN"
