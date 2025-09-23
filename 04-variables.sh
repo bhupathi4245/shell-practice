1a1419d173837ba85477b5c867d24766290150e1
Satish <sat116@gmail.com>
2025-09-23 18:16:36 -0400

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=MODI
+PERSON2=MUSK
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello Mr. $PERSON1, I am fine. How are you doing? "
+echo "$PERSON1:: I am fine too. What's up?"
+echo "$PERSON2:: Nothing, just going to Mars now, will calyou come"
+echo "$PERSON1:: Sorry, you carry on! I will come with your next visit"
---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello Mr. $PERSON1, I am fine. How are you doing? "
+echo "$PERSON1:: I am fine too. What's up?"
+echo "$PERSON2:: Nothing, just going to Mars now, will calyou come"
+echo "$PERSON1:: Sorry, you carry on! I will come with your next visit"
