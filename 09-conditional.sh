48921df69d07550644c40cd1aec587fe4c04b81a
Satish <sat116@gmail.com>
2025-09-23 21:07:34 -0400

    hello world started

--- 09-conditional.sh
+++ 09-conditional.sh
@@ -2,6 +2,7 @@
 
 if [$1 -lt 10] 
 then 
-echo "number entered is $1 less than 10"
+	echo "number entered is $1 less than 10"
 else 
-echo "number entered is $1 is not less than 10"
+	echo "number entered is $1 is not less than 10"
+fi
